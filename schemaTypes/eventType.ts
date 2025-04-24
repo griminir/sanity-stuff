8bfe2a0d1b670a77e568f76513f935b824f6f43c
griminir <viktor.elliot.king@gmail.com>
2025-04-24 12:24:39 +0200

    messing around with some validation and properties of the fields

--- schemaTypes/eventType.ts
+++ schemaTypes/eventType.ts
@@ -11,11 +11,27 @@ export const eventType = defineType({
     }),
     defineField({
       name: 'slug',
-      type: 'slug'
+      type: 'slug',
+      options: {
+        source: 'name'
+      },
+      validation: (rule) => rule.required().error(`required to generate website`),
+      hidden: ({document}) => !document?.name
     }),
     defineField({
       name: 'eventType',
-      type: 'string'
+      type: 'string',
+      options: {
+        list: ['in-person', 'virtual'],
+        layout: 'radio'
+      },
+        initialValue: 'in-person',
+      validation: (rule) => rule.custom((value, context)  => {
+        if (context?.document?.venue && value === 'virtual') {
+          return `You cannot select "virtual" if it's at a venue`;
+        }
+        return true;
+      })
     }),
     defineField({
       name: 'date',
@@ -23,13 +39,24 @@ export const eventType = defineType({
     }),
     defineField({
       name: 'doorsOpen',
-      type: 'number'
+      description: 'numbers of minutes before the event',
+      type: 'number',
+      initialValue: 60
     }),
     defineField({
       name: 'venue',
       type: 'reference',
-      to: [{type: 'venue'}]
-    }),defineField({
+      to: [{type: 'venue'}],
+      validation: (rule) => rule.custom((value, context)  => {
+        if (value && context?.document?.eventType === 'virtual') {
+          return 'only in-person events can have a venue'
+        }
+        return true;
+      }),
+      hidden: (context) => context?.document?.eventType === 'virtual',
+      // readOnly: ({value, document}) => !value && document?.eventType === 'virtual'
+    }),
+    defineField({
       name: 'headline',
       type: 'reference',
       to: [{type: 'artist'}]
